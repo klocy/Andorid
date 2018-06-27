89d89d1083265bc4c482a5f6d63380ff56ce8ea4
이현규\Hyun Gyu <dlgusrb0813@gmail.com>
2018-06-27 22:02:18 +0900

    reservation 추가

---
+++ MyApplication/app/src/main/java/org/androidtown/myapplication/ReservationActivity.java
@@ -0,0 +1,22 @@
+package org.androidtown.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class ReservationActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservation);
+    }
+
+
+    public void onButton1Clicked(View v) {
+        Toast.makeText(getApplicationContext(), "시간 입력", Toast.LENGTH_LONG).show();
+    }
+
+
+}
