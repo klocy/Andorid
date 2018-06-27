eb03c6b475c00e88d80989329b3f83e1d305da9a
LAPTOP-0BASJFMR\gksml <gksml96@gmail.com>
2018-06-27 22:40:04 +0900

    no message

--- MyApplication/app/src/main/java/org/androidtown/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/org/androidtown/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package org.androidtown.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +15,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onButton1Clicked(View v) {
-        Toast.makeText(getApplicationContext(), "지갑 계정을 생성 완료했습니다.", Toast.LENGTH_LONG).show();
+        // 지갑 생성 액티비티
+        Intent intent = new Intent(getApplicationContext(), WalletActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        startActivity(intent);
     }
+
+    public void onButton2Clicked(View v) {
+        // 자전거 예약 액티비티
+    }
+
+    public void onButton3Clicked(View v) {
+        // 자전거 대여 액티비티
+    }
+
 }
