c06424dffabc9e864d066e404a410df8e3c5bf21
이현규\Hyun Gyu <dlgusrb0813@gmail.com>
2018-06-28 01:33:50 +0900

    Intent

--- MyApplication/app/src/main/java/org/androidtown/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/org/androidtown/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,15 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn1 =(Button) findViewById(R.id.button1);
+
+        btn1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent  intent = new Intent(getApplicationContext(), WalletActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
-    public void onButton1Clicked(View v) {
-        // 지갑 생성 액티비티
-        Intent intent = new Intent(getApplicationContext(), WalletActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        startActivity(intent);
-    }
 
     public void onButton2Clicked(View v) {
         // 자전거 예약 액티비티
--- MyApplication/app/src/main/java/org/androidtown/myapplication/WalletActivity.java
+++ MyApplication/app/src/main/java/org/androidtown/myapplication/WalletActivity.java
@@ -10,7 +10,7 @@ public class WalletActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_wallet);
     }
 
     public void onButton1Clicked(View v) {
