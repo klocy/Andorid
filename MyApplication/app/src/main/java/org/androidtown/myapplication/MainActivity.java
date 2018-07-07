5e6ed1c25a3086f0a7f5d94bd99ee13deff9c5db
이현규\Hyun Gyu <dlgusrb0813@gmail.com>
2018-07-07 20:09:33 +0900

    main Imagebutton추가

--- MyApplication/app/src/main/java/org/androidtown/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/org/androidtown/myapplication/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,9 +15,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn1 =(Button) findViewById(R.id.button1);
-        Button btn2 =(Button) findViewById(R.id.button2);
-        Button btn3 = (Button) findViewById(R.id.button3);
+
+        ImageButton btn1 = (ImageButton) findViewById(R.id.create);
+        ImageButton btn2 = (ImageButton) findViewById(R.id.book);
+        ImageButton btn3 = (ImageButton) findViewById(R.id.rental);
 
         btn1.setOnClickListener(new View.OnClickListener(){
             @Override
