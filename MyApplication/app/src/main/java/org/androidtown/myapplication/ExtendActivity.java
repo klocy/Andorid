0506d820676f414cb89833a50148761e6bc35a75
LAPTOP-0BASJFMR\gksml <gksml96@gmail.com>
2018-08-11 01:56:56 +0900

    툴바 색상 변경

--- MyApplication/app/src/main/java/org/androidtown/myapplication/ExtendActivity.java
+++ MyApplication/app/src/main/java/org/androidtown/myapplication/ExtendActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -17,6 +20,15 @@ public class ExtendActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_extend);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true); //커스터마이징 하기 위해 필요
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼, 디폴트로 true만 해도 백버튼이 생김
+        //actionBar.setHomeAsUpIndicator(R.drawable.ic_back); //뒤로가기 버튼을 본인이 만든 아이콘으로 하기 위해 필요
 
 
         ImageButton btn = (ImageButton) findViewById(R.id.extend);
@@ -80,5 +92,16 @@ public class ExtendActivity extends AppCompatActivity {
 
         text.setText(String.valueOf(time));
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{
+                finish();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
